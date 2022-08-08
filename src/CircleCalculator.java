583b757d7be16c3251b6c5f61ff91f4b773ac65f
SKhn <shafaqmail1@gmail.com>
2022-08-08 16:29:18 +0100

    Initial commit

---
+++ src/CircleCalculator.java
@@ -0,0 +1,17 @@
+public class CircleCalculator {
+    public static void main(String[] args) {
+        //declare and initialize the variable to store radius value
+        double circleRadius=12.0;
+        // calculating circle diameter
+        double circleDiameter=2*circleRadius;
+        //calculating circle circumference
+        double pi=3.14159;
+        double circleCircumference=2*pi*circleRadius;
+        //calculating area of the circle
+        double circleArea=pi*circleRadius*circleRadius;
+        //Print the results
+        System.out.println("Diameter: "+circleDiameter);
+        System.out.println("Circumference: "+circleCircumference);
+        System.out.println("Area: "+circleArea);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    //A program to calculate the perimeter, area of the rectangle
+    public static void main(String[] args) {
+        //Declare and initialize the variables
+        double width=13.5;
+        double height=25.3;
+        double perimeter;
+        System.out.println("Width: "+width);
+        System.out.println("Height: "+height);
+        perimeter=2* (width+height);
+        System.out.println("The Perimeter is: "+perimeter);
+        //Calculating Area of the rectangle
+        double area=width*height;
+        System.out.println("Area: "+area);
+
+    }
+}
